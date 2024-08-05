8eea6222b9e97f464a8a3f2177710859b8af5b9f
Lindo-code <sdrowvieli1@gmail.com>
2024-08-05 11:03:23 +0200

    initial push

---
+++ my_script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
+// USE TO GET LOCATION
+function getLocation() {
+   
+}
+
+// USE TO DISPLAY POSITION
+function showPosition(position) {
+   
+}
+
+// HANDLE ERRORS
+function showError(error) {
+   
+}
+
+// RETRIEVE WEATHER DATA FROM API
+function getWeather(lat, lon) {
+   
+}
+
+// RETIEVE NEWS FROM API
+function getNews() {
+   
+}
---
+++ secrets.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export WEATHER_API_KEY=
+export NEWS_API_KEY=
