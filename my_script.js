d3df257a431d6a37f0d35e783a29378b0a37a867
Lindo-code <sdrowvieli1@gmail.com>
2024-08-05 13:25:48 +0200

    mistakenly uploaded keys

--- my_script.js
+++ my_script.js
@@ -1,30 +1,36 @@
 const axios = require("axios");
-
+const urls = {
+    weather: (lat, lon) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
+    news: `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+}
 document.addEventListener('DOMContentLoaded', () => {
-    
+    retrieveLocation()
 });
 
 // USE TO GET LOCATION
-function getLocation() {
-   
+function retrieveLocation() {
+    (navigator.geolocation) ? navigator.geolocation.getCurrentPosition(showPosition) : document.getElementById('weather-data').textContent = "Geolocation is not supported by this browser.";
 }
 
 // USE TO DISPLAY POSITION
-function showPosition(position) {
-   
+function showPosition(myPosition) {
+   const latitude = myPosition.coords.latitude, longitude = myPosition.coords.longitude;
+   retrieveWeather(latitude, longitude);
+   retrieveNews();
 }
 
 // HANDLE ERRORS
-function showError(error) {
+function displayErrors(error) {
    
 }
 
 // RETRIEVE WEATHER DATA FROM API
-function getWeather(lat, lon) {
-   
+async function retrieveWeather(latitude, longitude) {
+   const response = await axios.get(urls.weather(latitude, longitude));
+   console.log(response.data);
 }
 
 // RETIEVE NEWS FROM API
-function getNews() {
+function retrieveNews() {
    
 }
--- secrets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export WEATHER_API_KEY=af8b8440182a240e16c7a1987b286f5f
-export NEWS_API_KEY=f05b7df383f445b5be99a40b0f379b58
