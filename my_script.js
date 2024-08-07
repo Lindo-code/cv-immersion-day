da77f5bd3523c2ef43efd68ebdaecca78a9405a6
Lindo-code <sdrowvieli1@gmail.com>
2024-08-07 13:14:59 +0200

    retrieved & added data to html

--- my_script.js
+++ my_script.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
+const WEATHER_API_KEY = "af8b8440182a240e16c7a1987b286f5f";
+const NEWS_API_KEY = "f05b7df383f445b5be99a40b0f379b58";
+
 const urls = {
-    weather: (lat, lon) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
-    news: `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+    weather: (lat, lon) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_API_KEY}`,
+    news: (country) => `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${NEWS_API_KEY}`
 }
 document.addEventListener('DOMContentLoaded', () => {
     retrieveLocation()
@@ -15,8 +18,7 @@ function retrieveLocation() {
 // USE TO DISPLAY POSITION
 function showPosition(myPosition) {
    const latitude = myPosition.coords.latitude, longitude = myPosition.coords.longitude;
-   retrieveWeather(latitude, longitude);
-   retrieveNews();
+   retrieveData(latitude, longitude);
 }
 
 // HANDLE ERRORS
@@ -25,12 +27,21 @@ function displayErrors(error) {
 }
 
 // RETRIEVE WEATHER DATA FROM API
-async function retrieveWeather(latitude, longitude) {
+async function retrieveData(latitude, longitude) {
    const response = await axios.get(urls.weather(latitude, longitude));
-   console.log(response.data);
+   await retrieveNews(response.data.sys.country);
+   console.log(response.data.sys.country);
 }
 
 // RETIEVE NEWS FROM API
-function retrieveNews() {
-   
+async function retrieveNews(country) {
+    const response = await axios.get(urls.news(country));
+    const news = response.data.articles.map(article => `
+        <article>
+            <h3>${article.title}</h3>
+            <a href="${article.url}" target="_blank">Read more</a>
+        </article>
+    `).join('');
+    // console.log(news)
+    document.getElementById('latest-news').innerHTML = news;
 }
